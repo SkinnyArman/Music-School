6b75f8ccaf8d2f9575f6ecf289b3c8a19045396e
Arman Amini <armanwithamini@gmail.com>
2024-01-16 01:28:11 +0330

    add student delete

--- src/routers/student.js
+++ src/routers/student.js
@@ -18,25 +18,42 @@ router.get("/students", async (req, res) => {
 });
 
 router.post("/students", async (req, res) => {
-  const student = new Student({ ...req.body, enrolledClassCount: 0 });
-  const branch = await Branch.findOne({ branchNumber: req.body.branchNumber });
-  if (!branch) {
-    res.status(400).send({
-      message: "Branch Id does not exist!",
-    });
-  }
-
   try {
+    const branch = await Branch.findOne({
+      branchNumber: req.body.branchNumber,
+    });
+    if (!branch) {
+      return res.status(400).send({
+        message: "Branch Id does not exist!",
+      });
+    }
+
     const student = new Student({
       ...req.body,
       enrolledClassCount: 0,
       branch: branch._id,
     });
     await student.save();
-    res.send(200);
+    res.sendStatus(200);
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
+  }
+});
+
+router.delete("/students/:id", async (req, res) => {
+  try {
+    const student = await Student.findByIdAndDelete(req.params.id);
+
+    if (!student) {
+      return res.status(404).send({
+        message: "Student not found!",
+      });
+    }
+
+    res.send({ message: "Student deleted successfully." });
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 

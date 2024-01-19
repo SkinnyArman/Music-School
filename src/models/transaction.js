b4e59400f6907c6ebe5fc7a946da15dcc6e62399
Arman Amini <armanwithamini@gmail.com>
2024-01-19 20:06:27 +0330

    add transaction

---
+++ src/models/transaction.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    payer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+    },
+    branch: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Branch",
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
+const mongoose = require("mongoose");
---
+++ src/routers/transaction.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const Transaction = require("../models/transaction");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+
+// POST route to create a new transaction with branchId, studentId, and amount
+router.post("/transactions", async (req, res) => {
+  try {
+    const { branchId, studentId, amount } = req.body;
+
+    const transaction = new Transaction({
+      payer: studentId,
+      branch: branchId,
+      amount,
+    });
+
+    await transaction.save();
+    res.status(201).json(transaction);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: "Failed to create transaction" });
+  }
+});
+
+router.get("/teachers", async (req, res) => {
+  const page = parseInt(req.query.page) || 1; // Default to first page
+  const pageSize = parseInt(req.query.pageSize) || 10; // Default page size
+  const skip = (page - 1) * pageSize;
+
+  try {
+    const transactions = await Transaction.find()
+      .populate("branch")
+      .populate("payer", "student")
+      .limit(pageSize)
+      .skip(skip);
+
+    const totalTransactions = await Transaction.countDocuments(); // Total number of teachers
+
+    res.send({
+      transactions,
+      totalPages: Math.ceil(totalTransactions / pageSize),
+      currentPage: page,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
